c6c81e70c3680c0b53ea6bdaffd7de42c6406fb1
Alex Silva <alexifb1985@gmail.com>
2021-07-30 13:33:09 -0300

    teste api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import api from "./services/api"
 
-function App() {
+
+export default function App() {
+  const [livros, setLivros] = useState([])
+
+  useEffect(() => {
+    api.get("/")
+      .then(({ data }) => {
+        setLivros(data.livros);
+        console.log(livros)
+
+      })
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps 
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
-export default App;
